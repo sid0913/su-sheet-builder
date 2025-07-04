e232638b858f15ccdb5dc6ff85c02ba598634a78
sid0913 <siddharthpreetham09@gmail.com>
2025-07-04 08:39:44 +0200

    rectified the shp conversion code to make it correctly take the su top obj file reducing processing time from 20 minutes per su to 0.07 minutes per su

--- create_shp_files_in_bulk_from_volumetrics.py
+++ create_shp_files_in_bulk_from_volumetrics.py
@@ -7,7 +7,7 @@ import os
 from generate_top_shp import create_SU_shp_file
 from qgis.core import QgsApplication
 from processing.core.Processing import Processing
-
+import time
 
 #this is where QGS is installed
 QGS_PROGRAM_PATH = "C:\\Program Files\\QGIS 3.40.8"
@@ -38,55 +38,58 @@ def close_QGS(qgs):
 PATH = "C:\\Users\\Photogrammetry"
 YEAR = 2025
 
-TOTAL_ITEMS = len(os.listdir(os.path.join(PATH, "Volumetrics_2025", "Trench 17000"))) + len(os.listdir(os.path.join(PATH, "Volumetrics_2025", "Trench 18000"))) + len(os.listdir(os.path.join(PATH, "Volumetrics_2025", "Trench 19000"))) 
-
+# TOTAL_ITEMS = len(os.listdir(os.path.join(PATH, "Volumetrics_2025", "SU Top OBJs"))) + len(os.listdir(os.path.join(PATH, "Volumetrics_2025", "Trench 18000"))) + len(os.listdir(os.path.join(PATH, "Volumetrics_2025", "Trench 19000"))) 
+TOTAL_ITEMS = len(os.listdir(os.path.join(PATH, "Volumetrics_2025", "SU Top OBJs"))) 
+items_processed = len([f for f in os.listdir(os.path.join(PATH, "GIS_2025", "3D_SU_Shapefiles")) if f.endswith(".shp")])
 print(f"Total items to process: {TOTAL_ITEMS}")
-
-items_processed = 0
+print(f"Total items already processed: {items_processed}")
 
 
 qgs = start_QGS()
 print("QGIS Application started successfully.")
 
 try:
-    for trench in [ f"Trench {i}" for i in range(17000, 20000, 1000) ]:
-        for su_obj in os.listdir(os.path.join(PATH, "Volumetrics_2025", trench)):
-            if su_obj.endswith(".obj"):
-                file_path = os.path.join(PATH, "Volumetrics_2025", trench, su_obj)
+    for su_obj in os.listdir(os.path.join(PATH, "Volumetrics_2025", "SU Top OBJs")):
+        if su_obj.endswith(".obj"):
+            file_path = os.path.join(PATH, "Volumetrics_2025", "SU Top OBJs", su_obj)
 
-                #check if the output file exists
-                output_file_path = os.path.join(PATH, "GIS_2025", "3D_SU_Shapefiles", f"SU_{su_obj.split('.')[0].split('_')[-1]}_EPSG_32632.shp")
-                if os.path.exists(output_file_path):    
-                    continue  # Skip if the shapefile already exists
-                
-                if not os.path.exists(file_path):
-                    print(f"ERROR: File {file_path} does not exist, skipping...")
-                    continue
+            su_num = su_obj.split('.')[0].split('_')[-2]
 
-                try:
-                    create_SU_shp_file({
-                        "obj_file": file_path,
-                        "output_file_path": os.path.join(PATH, "GIS_2025", "3D_SU_Shapefiles"),
-                        "su_number": int(su_obj.split(".")[0].split("_")[-1]),
-                        "year": YEAR
-                    })
-
-                    items_processed += 1
-                    percentage_complete = (items_processed / TOTAL_ITEMS) * 100
-                    print(f"Processed {items_processed}/{TOTAL_ITEMS}: {su_obj}")
-                    print(f"Processed {su_obj}... {percentage_complete:.2f}% complete")
-                except Exception as e:
-                    print(f"Error processing {su_obj}: {e}")
-                    # Log the error to a file
-                    with open(os.path.join(PATH, "AutomateRockMask", "shp_error_log.txt"), "a") as error_file:
-                        error_file.write(f"{datetime.now(pytz.timezone('Europe/Rome')).strftime("%Y-%m-%d %H:%M:%S")} -- Error processing {su_obj}: {e}\n")
-                    continue
-
-            else:
+            #check if the output file exists
+            output_file_path = os.path.join(PATH, "GIS_2025", "3D_SU_Shapefiles", f"SU_{su_num}_EPSG_32632.shp")
+            if os.path.exists(output_file_path):    
+                continue  # Skip if the shapefile already exists
+            
+            if not os.path.exists(file_path):
+                print(f"ERROR: File {file_path} does not exist, skipping...")
                 continue
 
+            try:
+                start = time.time()  # Start time for performance measurement
+                create_SU_shp_file({
+                    "obj_file": file_path,
+                    "output_file_path": os.path.join(PATH, "GIS_2025", "3D_SU_Shapefiles"),
+                    "su_number": int(su_num),
+                    "year": YEAR
+                })
+                time_elapsed = round((time.time() - start) / 60, 3)  # Calculate time elapsed in minutes
+                print(f"SU shapefile created in {time_elapsed} minutes for {su_obj}.")
+                items_processed += 1
+                percentage_complete = (items_processed / TOTAL_ITEMS) * 100
+                print(f"Processed {items_processed}/{TOTAL_ITEMS}: {su_obj}")
+                print(f"Processed {su_obj}... {percentage_complete:.2f}% complete")
+            except Exception as e:
+                print(f"Error processing {su_obj}: {e}")
+                # Log the error to a file
+                with open(os.path.join(PATH, "AutomateRockMask", "shp_error_log.txt"), "a") as error_file:
+                    error_file.write(f"{datetime.now(pytz.timezone('Europe/Rome')).strftime("%Y-%m-%d %H:%M:%S")} -- Error processing {su_obj}: {e}\n")
+                continue
+
+        else:
+            continue
+
     print()
-    print("All items processed successfully!")
+    print("All items processed successfully in!")
     print(f"Processed {items_processed} items out of {TOTAL_ITEMS}.")
 
 finally:
